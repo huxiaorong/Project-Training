94c92000cbe17734d6003399c164723c6ab8e938
masuli5 <2022729972@qq.com>
2020-05-17 16:06:48 +0800

    ma

--- 马素隶/LeaChinese/src/com/chinese/entity/Word.java
+++ 马素隶/LeaChinese/src/com/chinese/entity/Word.java
@@ -1,6 +1,5 @@
 package com.chinese.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,16 +9,68 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "word")
 public class Word {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	private int id;
     private String word;
     private String pinyin;
+    private String audio;
     private String explanation;
-    private String tag;
+    private String sentence1;
+    private String pinyin1;
+    private String sentence2;
+    private String pinyin2;
+    private String sentence3;
+    private String pinyin3;
+    private int tag;
     private int findlevel;
     private String likeword;
 
+    public Word(){
+
+    }
+    
+    
+    
+    public Word(int id, String word, String pinyin, String audio, String explanation, String sentence1, String pinyin1,
+			String sentence2, String pinyin2, String sentence3, String pinyin3, int tag) {
+		super();
+		this.id = id;
+		this.word = word;
+		this.pinyin = pinyin;
+		this.audio = audio;
+		this.explanation = explanation;
+		this.sentence1 = sentence1;
+		this.pinyin1 = pinyin1;
+		this.sentence2 = sentence2;
+		this.pinyin2 = pinyin2;
+		this.sentence3 = sentence3;
+		this.pinyin3 = pinyin3;
+		this.tag = tag;
+	}
+
+
+
+	public Word(int id, String word, String pinyin, String audio, String explanation, String sentence1, String pinyin1,
+			String sentence2, String pinyin2, String sentence3, String pinyin3, int tag, int findlevel,
+			String likeword) {
+		super();
+		this.id = id;
+		this.word = word;
+		this.pinyin = pinyin;
+		this.audio = audio;
+		this.explanation = explanation;
+		this.sentence1 = sentence1;
+		this.pinyin1 = pinyin1;
+		this.sentence2 = sentence2;
+		this.pinyin2 = pinyin2;
+		this.sentence3 = sentence3;
+		this.pinyin3 = pinyin3;
+		this.tag = tag;
+		this.findlevel = findlevel;
+		this.likeword = likeword;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     public int getId() {
         return id;
     }
@@ -44,6 +95,14 @@ public class Word {
         this.pinyin = pinyin;
     }
 
+    public String getAudio() {
+        return audio;
+    }
+
+    public void setAudio(String audio) {
+        this.audio = audio;
+    }
+
     public String getExplanation() {
         return explanation;
     }
@@ -52,17 +111,69 @@ public class Word {
         this.explanation = explanation;
     }
 
-    public String getTag() {
+    public String getSentence1() {
+        return sentence1;
+    }
+
+    public void setSentence1(String sentence1) {
+        this.sentence1 = sentence1;
+    }
+
+    public String getSentence2() {
+        return sentence2;
+    }
+
+    public void setSentence2(String sentence2) {
+        this.sentence2 = sentence2;
+    }
+
+    public String getSentence3() {
+        return sentence3;
+    }
+
+    public void setSentence3(String sentence3) {
+        this.sentence3 = sentence3;
+    }
+    public int getTag() {
         return tag;
     }
 
-    public void setTag(String tag) {
+    public void setTag(int tag) {
         this.tag = tag;
     }
+    public String getPinyin1() {
+        return pinyin1;
+    }
 
-   
+    public void setPinyin1(String pinyin1) {
+        this.pinyin1 = pinyin1;
+    }
 
-    public int getFindlevel() {
+    public String getPinyin2() {
+        return pinyin2;
+    }
+
+    public void setPinyin2(String pinyin2) {
+        this.pinyin2 = pinyin2;
+    }
+
+    public String getPinyin3() {
+        return pinyin3;
+    }
+
+    public void setPinyin3(String pinyin3) {
+        this.pinyin3 = pinyin3;
+    }
+
+	@Override
+	public String toString() {
+		return "Word [id=" + id + ", word=" + word + ", pinyin=" + pinyin + ", audio=" + audio + ", explanation="
+				+ explanation + ", sentence1=" + sentence1 + ", pinyin1=" + pinyin1 + ", sentence2=" + sentence2
+				+ ", pinyin2=" + pinyin2 + ", sentence3=" + sentence3 + ", pinyin3=" + pinyin3 + ", tag=" + tag
+				+ ", findlevel=" + findlevel + ", likeword=" + likeword + "]";
+	}
+
+	public int getFindlevel() {
 		return findlevel;
 	}
 
@@ -71,18 +182,12 @@ public class Word {
 	}
 
 	public String getLikeword() {
-        return likeword;
-    }
-
-    public void setLikeword(String likeword) {
-        this.likeword = likeword;
-    }
-
-	@Override
-	public String toString() {
-		return "Word [id=" + id + ", word=" + word + ", pinyin=" + pinyin + ", explanation=" + explanation + ", tag="
-				+ tag + ", findlevel=" + findlevel + ", likeword=" + likeword + "]";
+		return likeword;
 	}
 
-   
+	public void setLikeword(String likeword) {
+		this.likeword = likeword;
+	}
+
+    
 }
